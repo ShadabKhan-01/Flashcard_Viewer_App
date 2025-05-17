1ef28e96250520bdfaef3fda17692ddb40ea84be
Shadab Khan <iam.shadab.khan.1001@gmail.com>
2025-05-17 16:13:39 +0530

    logic created

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,8 +20,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-white`}
+        
+      ><div className="fixed inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+        </div>
         {children}
       </body>
     </html>
